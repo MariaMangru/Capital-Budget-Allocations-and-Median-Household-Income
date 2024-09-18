3a39cdd49f255410fd90a50c09b22a6d49309659
MariaMangru <94894368+MariaMangru@users.noreply.github.com>
2024-09-18 00:45:48 -0400

    cleaned budget data to remove rows with total

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -8,37 +8,26 @@
 # Any other information needed? [...UPDATE THIS...]
 
 #### Workspace setup ####
+library(dplyr)
 library(tidyverse)
+library(readr)
 
 #### Clean data ####
-raw_data <- read_csv("data/raw_data/plane_data.csv")
+# clean ward profile 
+ward_profile <- read_csv("data/raw_data/2023-WardProfiles-2011-2021-CensusData.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# remove trailing whitespaces
+ward_profile <- ward_profile %>%
+  mutate(across(where(is.character), str_trim))
+
+
+# clean budget data 
+budget_data <- read_csv("data/raw_data/2022-2031-capital-budget-and-plan-details.csv")
+
+filtered_budget_data <- budget_data[-(which(budget_data$'Ward Number' %in% "CW")),]
+
+view(filtered_budget_data)
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(filtered_budget_data, "data/analysis_data/test.csv")
