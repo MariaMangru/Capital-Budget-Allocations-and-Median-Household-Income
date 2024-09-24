9cc628aae9bf2bf2337e45b901f554e6d5208e47
Maria Mangru <mangrumaria@gmail.com>
2024-09-24 00:00:52 -0400

    formatted data cleaning code so that it's all in one file

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -10,25 +10,44 @@ library(dplyr)
 library(tidyverse)
 library(readr)
 
+
 #### Data Cleaning ####
-# clean ward profile 
+
+## Ward Profile Data ## 
+# Clean ward profile 
 ward_profile <- read_csv("data/raw_data/2023-WardProfiles-2011-2021-CensusData.csv")
 
-# remove trailing whitespaces
-ward_profile <- ward_profile %>%
+# Remove trailing whitespaces from character columns
+ward_profile_clean <- ward_profile |>
   mutate(across(where(is.character), str_trim))
 
+# Save cleaned ward profile data 
+write_csv(ward_profile_clean, "data/analysis_data/2023-WardProfile-Data.csv")
 
-# clean budget data 
+
+## 2022 - 2031 Budget Data ##
+# Clean budget data 
 budget_data <- read_csv("data/raw_data/2022-2031-capital-budget-and-plan-details.csv")
 
+# Remove rows where 'Ward Number' is "CW"
 filtered_budget_data <- budget_data[-(which(budget_data$'Ward Number' %in% "CW")),]
 
-view(filtered_budget_data)
+# Identify year columns
+year_cols <- as.character(2022:2031)
+
+# Function to clean amount 
+clean_funding <- function(x) {
+  x_clean <- ifelse(x == "-", "0", x)
+  x_clean <- str_replace_all(x_clean, ",", "")
+  as.numeric(x_clean)
+}
+
+# Apply the cleaning function to year columns
+budget_data_clean <- filtered_budget_data |>
+  mutate(across(all_of(year_cols), clean_funding))
 
 
-#### Save data ####
-write_csv(filtered_budget_data, "data/analysis_data/2022-2031-budget_data.csv")
-
+# Save cleaned budget data 
+write_csv(budget_data_clean, "data/analysis_data/2022-2031-budget_data.csv")
 
 
