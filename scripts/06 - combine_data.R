c508349e32420acce947cc384c48278db536f9a4
Maria Mangru <mangrumaria@gmail.com>
2024-09-24 01:26:53 -0400

    created a new script to combine datasets

---
+++ scripts/06 - combine_data.R
@@ -0,0 +1,14 @@
+#### Preamble ####
+# Purpose: Explores data in an attempt to understand it more
+# Author: Maria Mangru
+# Date: September 2024
+# Contact: maria.mangru@mail.utoronto.ca
+# License: MIT
+
+#### Workspace setup ####
+library(dplyr)
+library(tidyverse)
+library(readr)
+library(knitr)
+library(kableExtra)
+library(scales)
